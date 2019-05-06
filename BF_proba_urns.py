e7571b5957ef8633edc4472809eb3db640f56fd8
adrien51460 <48181401+adrien51460@users.noreply.github.com>
2019-05-06 17:58:52 +0200

    Create BF_proba_urns.py

---
+++ BF_proba_urns.py
@@ -0,0 +1,18 @@
+"""
+Brute-Forcing a classic probability excercice (from HackerRank)
+
+Problem: Pick up from each urn a ball (3 urns with a certain number of red and black balls).
+         What is the probability of getting 2 red balls and a black one ?
+"""
+
+import itertools
+from fractions import Fraction
+
+b, r = [1], [0]
+X = b*3 + r*4
+Y = b*4 + r*5
+Z = b*4 + r*4
+r = [(i)for i in itertools.product(X,Y,Z)] 
+e = list(map(lambda x : sum(x) == 1,r))
+a=Fraction(e.count(True),len(e))
+print(a)
