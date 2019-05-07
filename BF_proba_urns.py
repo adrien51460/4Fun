4c845227069bf795314b98a3cafe1d816f5c50a1
adrien51460 <48181401+adrien51460@users.noreply.github.com>
2019-05-07 11:54:26 +0200

    Update BF_proba_urns.py

--- BF_proba_urns.py
+++ BF_proba_urns.py
@@ -2,14 +2,14 @@
 Brute-Forcing a classic probability excercice (from HackerRank)
 
 Problem: Pick up from each urn a ball (3 urns (X,Y,Z) with a certain number of red and black balls).
-         What is the probability of getting 2 red balls and a black one ?
+         What is the probability of getting 2 red balls and a black one ? 
 """
 
 import itertools
 from fractions import Fraction
 
 b, r = [1], [0]
-X = b*3 + r*4
+X = b*3 + r*4 #First Urn: 3 of black color , 4 or red color
 Y = b*4 + r*5
 Z = b*4 + r*4
 r = [(i)for i in itertools.product(X,Y,Z)] 
